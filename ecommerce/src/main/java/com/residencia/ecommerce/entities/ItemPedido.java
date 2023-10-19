370f7b01d455b8ca738d3a30ace312e1538f4463
BernardoChrist <bernardochrist.adm@gmail.com>
2023-10-19 16:14:08 -0300

    relacionamento das entidades de pedido e produto

--- ecommerce/src/main/java/com/residencia/ecommerce/entities/ItemPedido.java
+++ ecommerce/src/main/java/com/residencia/ecommerce/entities/ItemPedido.java
@@ -1,5 +1,43 @@
 package com.residencia.ecommerce.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "item_pedido")
 public class ItemPedido {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_item_pedido")
+	private Long idItemPedido;
+	
+	@Column(name = "quantidade")
+	private Integer quantidade;
+	
+	@Column(name = "preco_venda")
+	private Double precoVenda;
+	
+	@Column(name = "percentual_desconto")
+	private Double percentualDesconto;
+	
+	@Column(name = "valor_bruto")
+	private Double valorBruto;
+	
+	@Column(name = "valor_liquido")
+	private Double valorLiquido;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_produto", referencedColumnName = "id_produto")
+	private Produto produto;
+	
+	//falta inserir o ManyToMany
+	
+	
 }
--- ecommerce/src/main/java/com/residencia/ecommerce/entities/Pedido.java
+++ ecommerce/src/main/java/com/residencia/ecommerce/entities/Pedido.java
@@ -7,6 +7,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -17,20 +19,95 @@ public class Pedido {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id_pedido")
 	private Long idPedido;
-	
-	@Column(name = "data_pedido")
+
+	@Column(name = "data_pedido", nullable = false)
 	private Date dataPedido;
-	
+
 	@Column(name = "data_entrega")
 	private Date dataEntrega;
-	
+
 	@Column(name = "data_envio")
 	private Date dataEnvio;
-	
+
 	@Column(name = "status")
 	private String status;
-	
-	@Column(name = "valor_total")
+
+	@Column(name = "valor_total", nullable = false)
 	private Double valorTotal;
-	
+
+	@ManyToOne
+	@JoinColumn(name = "id_cliente", referencedColumnName = "id_cliente")
+	private Cliente cliente;
+
+	// constructors
+	public Pedido() {
+	}
+
+	public Pedido(Long idPedido, Date dataPedido, Date dataEntrega, Date dataEnvio, String status, Double valorTotal,
+			Cliente cliente) {
+		this.idPedido = idPedido;
+		this.dataPedido = dataPedido;
+		this.dataEntrega = dataEntrega;
+		this.dataEnvio = dataEnvio;
+		this.status = status;
+		this.valorTotal = valorTotal;
+		this.cliente = cliente;
+	}
+
+	// get e set
+	public Long getIdPedido() {
+		return idPedido;
+	}
+
+	public void setIdPedido(Long idPedido) {
+		this.idPedido = idPedido;
+	}
+
+	public Date getDataPedido() {
+		return dataPedido;
+	}
+
+	public void setDataPedido(Date dataPedido) {
+		this.dataPedido = dataPedido;
+	}
+
+	public Date getDataEntrega() {
+		return dataEntrega;
+	}
+
+	public void setDataEntrega(Date dataEntrega) {
+		this.dataEntrega = dataEntrega;
+	}
+
+	public Date getDataEnvio() {
+		return dataEnvio;
+	}
+
+	public void setDataEnvio(Date dataEnvio) {
+		this.dataEnvio = dataEnvio;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Double getValorTotal() {
+		return valorTotal;
+	}
+
+	public void setValorTotal(Double valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
 }
